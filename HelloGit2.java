8cc8742532fda920e6e33e2ec4164756175aa64e
JuanHF <juanmousen@gmail.com>
2025-11-05 19:50:54 -0600

    Se agrega una segunda linea

--- HelloGit2.java
+++ HelloGit2.java
@@ -1,5 +1,6 @@
 public class HelloGit2 {
     public static void main(String[] args) {
         System.out.println("new Hello Git 2!");
+        System.out.println("Another line in Hello Git 2");
     }
 }
