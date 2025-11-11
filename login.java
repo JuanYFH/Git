ce9c5fe1ec4812d629f9d7978d51cb4bbfcfc458
JuanHF <juanmousen@gmail.com>
2025-11-11 15:02:57 -0600

    Edit login from main

--- login.java
+++ login.java
@@ -2,5 +2,6 @@ public class login {
     public static void main(String[] args) {
         System.out.println("Login successful!");
         System.out.println("Estoy trabajando en la rama login");
+        System.out.println("Nueva línea añadida en la rama main");
     }
 }
